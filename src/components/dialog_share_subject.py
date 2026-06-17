dd3394d51ce94632eaede233e321dd6181943409
Ronakkudal <ronakkudal@gmail.com>
2026-06-17 22:09:33 +0530

    update app url

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -7,7 +7,7 @@ import io
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
     #Change after deployment
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "snapclass-smartattendance-system.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
